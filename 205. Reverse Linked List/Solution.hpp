90f70eec7bd90ef06df20cd356b814eda043b59c
wtlusvm <wtlusvm@users.noreply.github.com>
2018-04-06 17:30:57 +0800

    Add a solution for Q205

--- 205. Reverse Linked List/Solution.hpp
+++ 205. Reverse Linked List/Solution.hpp
@@ -1,20 +1,18 @@
 #include <stack>
-
-// Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    explicit ListNode(int x) : val(x), next(nullptr)
-    {}
-};
+#include "../Unity/ListNode.hpp"
 
 class Solution {
 public:
-    ListNode *reverseList(ListNode *head)
+    ListNode *reverseList1(ListNode *head)
     {
-        ListNode *temp = head;
+        if (head == nullptr)
+        {
+            return head;
+        }
+
+        // 利用栈先进后出的特点将链表反转
         std::stack<int> si;
+        ListNode *temp = head;
         while (temp != nullptr)
         {
             si.push(temp->val);
@@ -29,6 +27,24 @@ public:
             temp = temp->next;
         }
 
+        return head;
+    }
+
+    ListNode *reverseList2(ListNode *head)
+    {
+        if (head == nullptr)
+        {
+            return head;
+        }
+        ListNode *Current = head->next;
+        head->next = nullptr;
+        while (Current != nullptr)
+        {
+            ListNode *Temp = Current->next;
+            Current->next = head;
+            head = Current;
+            Current = Temp;
+        }
         return head;
     }
 };
--- 205. Reverse Linked List/TEST.cc
+++ 205. Reverse Linked List/TEST.cc
@@ -8,7 +8,7 @@ TEST_CASE("Reverse Linked List", "[reverseList]")
 
     SECTION("robust")
     {
-        REQUIRE(s.reverseList(nullptr) == nullptr);
+        REQUIRE(s.reverseList2(nullptr) == nullptr);
     }
 
     SECTION("common")
@@ -18,7 +18,7 @@ TEST_CASE("Reverse Linked List", "[reverseList]")
         ListNode l3(3);
         l1.next=&l2;
         l2.next=&l3;
-        s.reverseList(&l1);
+        s.reverseList2(&l1);
         REQUIRE(l1.val==3);
         REQUIRE(l2.val==2);
         REQUIRE(l3.val==1);
